f58f5411ef6fb469a721307c1d5362af79d203ed
C12H22O12 <director20844@gmail.com>
2021-06-30 16:52:33 +0900

    21/06/30 백그라운드 디자인 결정

--- src/component/Presentaional/AllHeroes.js
+++ src/component/Presentaional/AllHeroes.js
@@ -1,18 +1,21 @@
 import React from "react";
+import "./css/AllHeroes.css"
 
 function AllHeroes() {
-  return (
-    <div>
-      <div>
-          <span>
-            <h1>영웅들 모아보기</h1>
-          </span>
-          <span>
-            <p>ㅁㅁ</p>
-          </span>
-      </div>
-    </div>
-  );
+    return (
+        <div id="wholeFrame">
+            <div id="seeAll">
+                <div id="showHero">
+                    <h1>영웅들 모아보기</h1>
+                    <input type="text" placeholder="영웅 이름을 입력하세요"></input>
+                    <input type="button" value="검색"/>
+                </div>
+                <div id="heroList">
+                    <h2>라이레이</h2>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default AllHeroes;
