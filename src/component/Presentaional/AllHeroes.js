311c7db018a18f0491502048cda32412546fde2b
C12H22O12 <director20844@gmail.com>
2021-07-01 00:41:24 +0900

    seeAll 정립, 폰트 적용

--- src/component/Presentaional/AllHeroes.js
+++ src/component/Presentaional/AllHeroes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LOGO from "../img/메인로고.png"
+import search from "../img/icon/search.png"
 import "./css/AllHeroes.css"
 
 function AllHeroes() {
@@ -8,9 +9,12 @@ function AllHeroes() {
             <div id="seeAll">
                 <div id="showHero">
                     <img src={LOGO}/>
-                    <h1>영웅들 모아보기</h1>
-                    <input id = "search" type="text" placeholder="영웅 이름을 입력하세요"/>
-                    <input id = "searchBtn" type="image" value="검색"/>
+                    <h1>기사들 모아보기</h1>
+                    <p>찾고싶은 기사가 있다면?</p>
+                    <div>
+                        <input id="search" type="text" placeholder="영웅 이름을 입력하세요"/>
+                        <input id="searchBtn" type="image" src={search}/>
+                    </div>
                 </div>
                 <div id="heroList">
                     <h2>라이레이</h2>
