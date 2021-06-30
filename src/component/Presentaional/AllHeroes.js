7e08e516dce0c530744a89c6c5593f0917a0ae65
C12H22O12 <director20844@gmail.com>
2021-06-30 23:59:16 +0900

    AllHeroes 인터페이스 작업

--- src/component/Presentaional/AllHeroes.js
+++ src/component/Presentaional/AllHeroes.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LOGO from "../img/메인로고.png"
 import "./css/AllHeroes.css"
 
 function AllHeroes() {
@@ -6,9 +7,10 @@ function AllHeroes() {
         <div id="wholeFrame">
             <div id="seeAll">
                 <div id="showHero">
+                    <img src={LOGO}/>
                     <h1>영웅들 모아보기</h1>
-                    <input type="text" placeholder="영웅 이름을 입력하세요"></input>
-                    <input type="button" value="검색"/>
+                    <input id = "search" type="text" placeholder="영웅 이름을 입력하세요"/>
+                    <input id = "searchBtn" type="image" value="검색"/>
                 </div>
                 <div id="heroList">
                     <h2>라이레이</h2>
