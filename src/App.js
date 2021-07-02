60b11dc8d2bd8f44cc177e5ddd6a96c8b997b3c8
C12H22O12 <director20844@gmail.com>
2021-07-02 22:26:17 +0900

    21/07/02 aboutHero 기본 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Heroes from './component/Presentaional/AllHeroes'
+import Heroes from './component/Presentaional/AboutHero'
 
 function App() {
   return (
---
+++ src/component/Presentaional/AboutHero.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../img/Heroes/LAIREI.png"
+import "./css/AboutHero.css"
+
+function AllHeroes() {
+    return (
+        <div id="BackgroundFrame">
+            <div id="info">
+                <div id="heroImg">
+                    <img src={Hero}/>
+                </div>
+                <div id="heroInfo">
+                    <p>용의 피에 부끄럽지 않은 영원한 충성을 바치겠어요.</p>
+                    <h1>라이레이</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AllHeroes;
