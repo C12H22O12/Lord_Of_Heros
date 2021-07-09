4fb8a32bcbe56285533d4098b78c382ab9183b5f
C12H22O12 <director20844@gmail.com>
2021-07-09 16:27:46 +0900

    21/07/09 hover 적용

--- src/component/Presentaional/Sidebar.js
+++ src/component/Presentaional/Sidebar.js
@@ -6,10 +6,10 @@ import Mu from "../img/icon/뮤.png"
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <img src={Mu}/>
+            <img src={Mu} id="mu"/>
             <ul className="barItem">
-                <li className="Item"><a href="#info">기본 스텟</a></li>
-                <li className="Item"><a href="#backgroundStory">백스토리</a></li>
+                <li className="Item"><a href="#info">스킬 계수</a></li>
+                <li className="Item"><a href="#backgroundStory"> 스토리</a></li>
                 <li className="Item"><a href="#skill">스킬</a></li>
                 <li className="Item"><a href="#talk">대사</a></li>
                 <li className="Item"><a>코스튬</a></li>
