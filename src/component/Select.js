21ae4aad0b49f4bbef1726879f0c20c198460dee
C12H22O12 <director20844@gmail.com>
2022-01-05 15:04:01 +0900

    220105 15:03 Subtitle 추가, Nation 작업 예정 (전체적인 틀부터)

---
+++ src/component/Select.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Select({list}) {
+    return(
+      <select>
+        <option>1</option>
+        <option>1</option>
+        <option>1</option>
+        <option>1</option>
+        <option>1</option>
+      </select>
+    )
+}
+
+
+export default Select;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,22 +1,20 @@
 import React from "react";
 import Countries from "../component/Countries";
-import Title from "../layout/Title";
+import Filter from "../layout/Filter";
+import Title from "../component/Title";
+import Nation from "../layout/Nation";
 
 function Home() {
-    const data = [
-        "속성별",
-        "불",
-        "물",
-        "대지",
-        "빛",
-        "암"
-    ];
     return (
         <div id="wholeFrame">
             <div className="container">
                 <div className="con_Header">
                     <Title cont={"영웅들 모아보기"}/>
                     <Countries />
+                    <Filter />
+                </div>
+                <div className="con_Body">
+                    <Nation />
                 </div>
             </div>
         </div>
