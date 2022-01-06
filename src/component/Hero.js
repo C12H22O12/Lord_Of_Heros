68a8be9a8c5c5a9d3fdfea654a3dd374ada86e91
C12H22O12 <director20844@gmail.com>
2022-01-06 17:26:24 +0900

    220106 17:26 Hero 틀 추가, 세부사항 고민

---
+++ src/component/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <div>type, name</div>
+      <div>img</div>
+      <div>class, how bring</div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/layout/Heroes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../component/Hero";
+
+function Heroes({cont}) {
+  return (
+    <div className="Heroes">
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+    </div>
+  );
+}
+
+export default Heroes;
